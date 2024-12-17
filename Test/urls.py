782e434c01a391ec2509238327baac66062551f5
sunghee <sungheenuri@gmail.com>
2024-12-17 14:51:41 +0900

    변경사항 커밋

--- Test/urls.py
+++ Test/urls.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from django.urls import path
 from Test import views
 
@@ -9,4 +10,18 @@ urlpatterns = [
     path("do/", views.do, name="do"),
     path("index/", views.index, name="index"),
     path("portfolio/", views.portfolio, name="portfolio"),
+=======
+from django.urls import path
+from Test import views
+
+app_name = "test"
+
+urlpatterns = [
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+    path("do/", views.do, name="do"),
+    path("index/", views.index, name="index"),
+    path("", views.index, name="home"),
+    path("portfolio/", views.portfolio, name="portfolio"),
+>>>>>>> 15ddfc0ab029985e33c5f3b7aeba4753d9837705
 ]
