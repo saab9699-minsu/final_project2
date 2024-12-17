0e8a1c24453c868430c2acc7e3f6cd7652254414
sunghee <sungheenuri@gmail.com>
2024-12-17 15:11:26 +0900

    merge 정리

--- Test/urls.py
+++ Test/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.urls import path
 from Test import views
 
@@ -10,18 +9,4 @@ urlpatterns = [
     path("do/", views.do, name="do"),
     path("index/", views.index, name="index"),
     path("portfolio/", views.portfolio, name="portfolio"),
-=======
-from django.urls import path
-from Test import views
-
-app_name = "test"
-
-urlpatterns = [
-    path("about/", views.about, name="about"),
-    path("contact/", views.contact, name="contact"),
-    path("do/", views.do, name="do"),
-    path("index/", views.index, name="index"),
-    path("", views.index, name="home"),
-    path("portfolio/", views.portfolio, name="portfolio"),
->>>>>>> 15ddfc0ab029985e33c5f3b7aeba4753d9837705
 ]
