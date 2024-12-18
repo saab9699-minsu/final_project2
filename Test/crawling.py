c0156715037218ece4e68413fec0326735f06d14
sunghee <sungheenuri@gmail.com>
2024-12-18 16:18:57 +0900

    민수_메인페이지 수정

--- Test/crawling.py
+++ Test/crawling.py
@@ -17,11 +17,15 @@ def news_crawling():
         response = requests.get(url, headers=headers)
         response.raise_for_status()  # HTTP 오류 발생 시 예외를 발생시킴
         soup = BeautifulSoup(response.text, "html.parser")
+        num = 0
         for i in soup.select("div.min-w-0 div > div > ul > li"):
+            if num > 4:
+                break
             title_list.append(i.select("a")[0].text.strip())
             company_list.append(i.select("li span")[1].text)
             date_list.append(i.select("li time")[0].text)
             href_list.append(i.select("a")[0]["href"])
+            num += 1
 
 
     except requests.exceptions.HTTPError as e:
@@ -39,3 +43,16 @@ def news_crawling():
 def upbit():
     df = pyupbit.get_ohlcv("KRW-BTC", interval = "mintue5", count = 24 * 12)
     return df
+
+def upbit2():
+    coin_list = ["KRW-BTC", "KRW-ETH", "KRW-XRP", "KRW-SOL", "KRW-DOGE"]
+    coin_name = ["비트코인(BTC)", "이더리움(ETH)", "리플(XRP)", "솔라나(SOL)", "도지코인(DOGE)"]
+    coin_data = []
+    for coin, name in zip(coin_list, coin_name):
+        btc_df = pyupbit.get_ohlcv(coin, interval = "day", count = 2)
+        coin_data.append(({
+            "name" : name,
+            "price" : btc_df["close"][1],
+            "change" : (btc_df["close"].pct_change() * 100)[1].round(2),
+        }))
+    return coin_data
