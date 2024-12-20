bba56ba40bba82b9be7d396409dcf74df88755a0
sunghee <sungheenuri@gmail.com>
2024-12-20 17:48:14 +0900

    프로펫 그래프 추가 완료~ (테이블 추가 예정)

--- Test/urls.py
+++ Test/urls.py
@@ -6,11 +6,13 @@ app_name = "test"
 urlpatterns = [
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
+    path("test/", views.prophet, name="prophet"),
+
     path("do/", views.do, name="do"),
     path("index/", views.index, name="index"),
     path("", views.index, name="home"), 
     path("portfolio/", views.portfolio, name="portfolio"),
 
     path("detail_halving_pattern/", views.halving_pattern, name="halving_pattern"),
-    path("detail_issue/", views.detail_issue, name="issue"),
+    path("detail_issue/", views.detail_issue, name="issue"),    
 ]
